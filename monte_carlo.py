a72b69f1fb9c39c59627484be8910542c3dbad86
rokaone <davis.hansen13@gmail.com>
2025-02-23 20:49:56 -0700

    Reworking rep code to use AWGN

--- monte_carlo.py
+++ monte_carlo.py
@@ -16,8 +16,11 @@ def repetition_code(symbol: int, n: int) -> list:
     return [symbol] * n
 
 
-def bpsk(symbol, symbol_energy) -> float:
-    return (1 - 2 * symbol) * symbol_energy
+def bpsk(symbol, symbol_energy) -> list:
+    symbols = []
+    for sym in symbol:
+        symbols.append((1 - 2 * sym) * symbol_energy)
+    return symbols
 
 
 class Generator:
@@ -35,9 +38,12 @@ class Channel:
         self.noise_power = noise_power
         self.alpha = [1 - alpha, alpha]
 
-    def apply_channel(self, symbol: float) -> float:
-        noise = np.sqrt(self.noise_power) * np.random.randn() + self.mean
-        return symbol + noise
+    def apply_channel(self, symbol: list) -> list:
+        symbols = []
+        for sym in symbol:
+            noise = np.sqrt(self.noise_power) * np.random.randn() + self.mean
+            symbols.append(sym + noise)
+        return symbols
 
     def flip_bit(self, symbols: list) -> list:
         return [int(np.abs(1 - sym)) if np.random.choice([0, 1], p=self.alpha) else sym for sym in symbols]
@@ -45,7 +51,13 @@ class Channel:
 
 class Decoder:
     def repetition_code(self, received: list) -> int:
-        return int(sum(received) > np.floor(len(received) / 2))
+        rec_dec = []
+        for rec in received:
+            if rec >= 0:
+                rec_dec.append(0)
+            else:
+                rec_dec.append(1)
+        return int(sum(rec_dec) > np.floor(len(rec_dec) / 2))
 
     def bpsk(self, mean: float, received: float) -> int:
         return int(received < mean)
@@ -67,35 +79,37 @@ class MonteCarlo:
         print("Starting trial n:", n)
         g = Generator([0, 1], [0.5, 0.5])
         dec = Decoder()
+        bit_energy = 1
         p_error = []
         p_error_diff = []
-        alphas = []
+        sigmas = []
         p_error_expected = []
         if (n >= 11):
             self.target_p_e = 1e-4
             N = self.calc_n_trials()
 
-        for alpha in range(5, 55, 5):
-            chan = Channel(0, 0.1, alpha / 100)
+        for sigma in range(50, 1000, 50):
+            chan = Channel(0, sigma / 100, 0)
             bit_err = 0
 
             for _ in range(N):
                 bit = g.gen()
                 bit_enc = repetition_code(bit, n)
-                bit_chan = chan.flip_bit(bit_enc)
+                bit_map = bpsk(bit_enc, bit_energy)
+                bit_chan = chan.apply_channel(bit_map)
                 bit_dec = dec.repetition_code(bit_chan)
                 if bit_dec != bit:
                     bit_err += 1
 
             p_e = bit_err / N
-            p_e_expected = ideal_rep_code(n, alpha / 100)
+            p_e_expected = ideal_rep_code(n, sigma / 100)
             p_error.append(p_e)
-            p_error_expected.append(p_e_expected)
+            p_error_expected.append(float(p_e_expected))
             p_error_diff.append(np.abs(p_e - p_e_expected))
-            alphas.append(alpha / 100)
+            sigmas.append(sigma / 100)
 
         print("Finished trial n:", n)
-        return n, alphas, p_error, p_error_diff  # Return results for plotting
+        return n, sigmas, p_error, p_error_expected  # Return results for plotting
 
     def run_trials_rep_code_parallel(self, repetitions: int) -> None:
         """Parallelized version of run_trials_rep_code"""
@@ -107,23 +121,27 @@ class MonteCarlo:
             results = pool.starmap(self.run_single_trial_rep_code, [(n, N) for n in n_values])
 
         plt.figure(1)
-        for n, alphas, p_error, _ in results:
-            plt.loglog(alphas, p_error, label=f"n: {n}", marker="x")
+        for n, sigmas, p_error, p_error_expected in results:
+            print(f"n: {n}")
+            print(f"{sigmas=}")
+            print(f"{p_error=}")
+            print('-'*20)
+            plt.loglog(sigmas, p_error, label=f"n: {n}", marker="x")
 
         plt.legend()
         plt.grid(True)
         plt.title("Probability of Error for Repetition Code")
-        plt.xlabel(r"$\alpha$")
+        plt.xlabel(r"$\sigma$")
         plt.ylabel(r"$P_e$")
 
         plt.figure(2)
-        for n, alphas, _, p_error_diff in results:
-            plt.loglog(alphas, p_error_diff, label=f"p_e error - n: {n}", marker="x")
+        for n, sigmas, _, p_error_diff in results:
+            plt.loglog(sigmas, p_error_diff, label=f"p_e error - n: {n}", marker="x")
 
         plt.legend()
         plt.grid(True)
         plt.title("Error in Probability of Error for Repetition Code")
-        plt.xlabel(r"$\alpha$")
+        plt.xlabel(r"$\sigma$")
         plt.ylabel(r"$Error(P_e)$")
 
     def run_single_trial_bpsk(self, sigma, N):
@@ -172,8 +190,8 @@ if __name__ == "__main__":
     mp.set_start_method("spawn", force=True)
 
     mc = MonteCarlo(0.9, 1e-2, 0.1)
-    #mc.run_trials_rep_code_parallel(repetitions=9)
-    mc.run_trials_bpsk_parallel()
+    mc.run_trials_rep_code_parallel(repetitions=9)
+    #mc.run_trials_bpsk_parallel()
     plt.show()
     print("Done")
 
